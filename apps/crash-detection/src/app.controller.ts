880ba857898c49e1046ba3a55c3ffac8359df337
Michael Julian Rechberger <rechberger.michael@aol.com>
2025-12-01 11:30:08 +0100

    feat(health-check): added health check endpoint and update app service method

--- apps/crash-detection/src/app.controller.ts
+++ apps/crash-detection/src/app.controller.ts
@@ -3,14 +3,20 @@ import { AppService } from './app.service';
 import { EventPattern } from '@nestjs/microservices';
 import { Topics } from 'lib/mqtt.topics';
 import { Logger } from '@nestjs/common';
+import { TrackReedEvent } from 'lib/events.types';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
   private readonly logger = new Logger(AppController.name, { timestamp: true });
 
+  @Get('health')
+  health(): number {
+    return this.appService.health();
+  }
+
   @EventPattern(Topics.entryTurnEvent())
-  async handleEntryTurnEvent(data: any) {
+  async handleEntryTurnEvent(data: TrackReedEvent) {
     this.logger.debug(`Received entry turn event: ${JSON.stringify(data)}`);
     // Handle the entry turn event logic here
   }
