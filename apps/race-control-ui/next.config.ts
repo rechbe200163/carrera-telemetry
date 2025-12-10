67ac5eec6ba2147b23e7e504e1c627006a6101ff
Michael Julian Rechberger <rechberger.michael@aol.com>
2025-12-10 11:19:50 +0100

    [frontend] added custom cachlife for statistics

--- apps/race-control-ui/next.config.ts
+++ apps/race-control-ui/next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
+  cacheLife: {
+    statistics: {
+      stale: 60 * 60 * 24 * 1, // 14 days
+      revalidate: 60 * 60 * 4, // 1 day
+      expire: 60 * 60 * 24 * 12, // 14 days
+    },
+  },
 };
 
 export default nextConfig;
