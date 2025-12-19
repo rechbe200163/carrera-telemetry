6933bb8b14d5a51e26e3e0060898678462ce97c3
Michael Julian Rechberger <rechberger.michael@aol.com>
2025-12-19 12:48:49 +0100

    [frontend] added laps api service

---
+++ apps/race-control-ui/lib/api/laps-api.service.ts
@@ -0,0 +1,23 @@
+// lib/api/laps-api.service.ts
+'server-only';
+
+import { apiClient } from '@/lib/api-client';
+import { Laps } from '../types';
+import { ENDPOINTS } from '../enpoints';
+import { cacheTag } from 'next/cache';
+import { CACHE_KEYS } from '../cache-keys';
+
+// ------------------------------------------------------
+//  GET ALL DRIVERS
+// ------------------------------------------------------
+export async function getLapBySessionByDriverId(
+  sessionId: number,
+  driverId: number
+): Promise<Laps[]> {
+  'use cache';
+  cacheTag(CACHE_KEYS.lapsSessionDriver(sessionId, driverId));
+
+  return apiClient.get<Laps[]>(
+    ENDPOINTS.LAPS.GET_BY_SESSION_BY_DRIVER(sessionId, driverId)
+  );
+}
